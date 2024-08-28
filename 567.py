ae52cca75fa2161b2977400d9eca1c7582843fa4
Huyen24303 <hp24032003@gmail.com>
2024-08-28 10:03:06 +0700

    Add files via upload

---
+++ 567.py
@@ -0,0 +1,8 @@
+chu_ngu = ["Anh", "Em"]
+dong_tu = ["Choi", "Yêu"]
+tan_ngu = ["Bóng đá", "Xếp hình"]
+
+for cn in chu_ngu:
+    for dt in dong_tu:
+        for tn in tan_ngu:
+            print(f"{cn} {dt} {tn}.")
